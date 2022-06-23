685183cfe70c8ba2de8779093f2772fdb95be8fb
ibizugbe <abrahamibizugbe@gmail.com>
2022-06-23 14:58:20 +0100

    fix linter errors

--- menu.js
+++ menu.js
@@ -1,21 +1,22 @@
-const menuOpen = document.querySelector(".hamburger-bar");
-const containerNav = document.querySelector(".container-nav");
-const menuClose = document.querySelector(".close-hamb");
-const navList = document.querySelector(".my-nav");
-const about = document.querySelectorAll(".mobile-nav-li");
+const menuOpen = document.querySelector('.hamburger-bar');
+const containerNav = document.querySelector('.container-nav');
+const menuClose = document.querySelector('.close-hamb');
+const navList = document.querySelector('.my-nav');
+const about = document.querySelectorAll('.mobile-nav-li');
 
-menuOpen.addEventListener("click", () => {
-  containerNav.style.display = "flex";
-  navList.style.display = "none";
+menuOpen.addEventListener('click', () => {
+  containerNav.style.display = 'flex';
+  navList.style.display = 'none';
 });
 
-menuClose.addEventListener("click", () => {
-  containerNav.style.display = "none";
-  navList.style.display = "flex";
+menuClose.addEventListener('click', () => {
+  containerNav.style.display = 'none';
+  navList.style.display = 'flex';
 });
 
 about.forEach((n) => {
-  n.addEventListener("click", () => {
-    containerNav.style.display = "none";
+  n.addEventListener('click', () => {
+    containerNav.style.display = 'none';
+    navList.style.display = 'flex';
   });
 });
