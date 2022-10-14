92f3b7bf1d4c762671b0ff55465883d3b7deeac2
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-10-14 10:18:01 +0100

    add custormer images

--- JS/main.js
+++ JS/main.js
@@ -1,45 +1,45 @@
 const comments = [
   {
-    userName: 'Keg Jonah',
-    userImage: '/Assets/other-images/IMG-20220504-WA0018.jpg',
+    userName: "Keg Jonah",
+    userImage: "/Assets/other-images/IMG-20220504-WA0018.jpg",
     comments:
-      'Best food delivery app i have used in nigeria. Absolutly recommended and ill use it over and over agai!',
+      "Best food delivery app i have used in nigeria. Absolutly recommended and ill use it over and over agai!",
   },
   {
-    userName: 'John Kingsley',
-    userImage: '/Assets/other-images/IMG-20220504-WA0018.jpg',
+    userName: "John Kingsley",
+    userImage: "/Assets/review-images/said.png",
     comments:
-      'Absolutely my favorite restaurant, everything came in ease and the food tastes really great.',
+      "Absolutely my favorite restaurant, everything came in ease and the food tastes really great.",
   },
   {
-    userName: 'James Hartley',
-    userImage: '/Assets/other-images/IMG-20220504-WA0018.jpg',
+    userName: "James Hartley",
+    userImage: "/Assets/review-images/chris.png",
     comments:
-      'I use chopLife frequently and I can say they never dissapoint. The delivery process is very smooth and transparent',
+      "I use chopLife frequently and I can say they never dissapoint. The delivery process is very smooth and transparent",
   },
   {
-    userName: 'Esosa Constance',
-    userImage: '/Assets/other-images/IMG-20220504-WA0018.jpg',
+    userName: "Esosa Constance",
+    userImage: "/Assets/review-images/Lade.jpg",
     comments:
-      'I always use chopLife for my family dinner, My family always asl for it. They are the best!',
+      "I always use chopLife for my family dinner, My family always asl for it. They are the best!",
   },
   {
-    userName: 'Austin Carnigie',
-    userImage: '/Assets/other-images/IMG-20220504-WA0018.jpg',
+    userName: "Austin Carnigie",
+    userImage: "/Assets/review-images/Nic.png",
     comments:
-      'I always use chopLife for my family dinner, My family always asl for it. They are the best!',
+      "I always use chopLife for my family dinner, My family always asl for it. They are the best!",
   },
   {
-    userName: 'Joshua Justin',
-    userImage: '/Assets/other-images/IMG-20220504-WA0018.jpg',
+    userName: "Joshua Justin",
+    userImage: "/Assets/review-images/piesie.png",
     comments:
-      'Absolutely my favorite restaurant, everything came in ease and the food tastes really great.',
+      "Absolutely my favorite restaurant, everything came in ease and the food tastes really great.",
   },
 ];
 
 comments.forEach((element) => {
-  const reviewContainer = document.querySelector('.reviews-container');
-  const reviewCard = document.createElement('div');
+  const reviewContainer = document.querySelector(".reviews-container");
+  const reviewCard = document.createElement("div");
   reviewCard.innerHTML = `
     <img
       src='${element.userImage}'
@@ -52,5 +52,5 @@ comments.forEach((element) => {
     </div>`;
 
   reviewContainer.appendChild(reviewCard);
-  reviewCard.classList.add('review-card');
+  reviewCard.classList.add("review-card");
 });
